006231d7b7a66ef7b3a16eba5842fe8e8c366aee
agialif <agialif8@gmail.com>
2020-12-20 19:02:32 +0800

    update

---
+++ lib/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  final Color colorPrimary = Color(0xFF1CA2A2);
+  final Color colorSecondary = Color(0xFF1CA25F);
+  final Color colorTertiary = Color(0xFF425195);
+}
